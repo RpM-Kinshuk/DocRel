169d9dbdfaaefd707f5b0dd5f7b21c5cf4feac7a
Kinshuk Goel <103813028+RpM-Kinshuk@users.noreply.github.com>
2024-09-20 03:48:52 +0530

    Improved speed

--- utils/sts.py
+++ utils/sts.py
@@ -10,6 +10,14 @@ disable_tqdm = True
 
 def similarity_scores(query, top_n, model, sim_measure):
     dataset = pd.read_csv('./scopus_results.csv')
+    if os.path.exists(f'./{model}_cosine_scores.csv'):
+        tmp_df = pd.read_csv('./cosine_scores.csv')
+        tmp_df.drop(columns=['Similarity'], inplace=True)
+        if tmp_df.equals(dataset):
+            tmp_str = 'cosine' if sim_measure == 'cosine' else 'euclid'
+            current_df = pd.read_csv(f'./{model}_{tmp_str}_scores.csv')
+            top_results = current_df.sort_values(by='Similarity', ascending=False if sim_measure == 'cosine' else True).head(int(top_n))
+            return top_results
     # dataset = dataset[:1000]
     # Make a list of str out of the abstracts
     abstracts = dataset['abstract'].astype(str).tolist()
@@ -29,8 +37,8 @@ def similarity_scores(query, top_n, model, sim_measure):
     cos_result_df = pd.concat([dataset, cos_scores_df], axis=1)
     euclid_result_df = pd.concat([dataset, euclid_scores_df], axis=1)
     if sv:
-        cos_result_df.to_csv(f'./cosine_scores.csv', index=False)
-        euclid_result_df.to_csv(f'./euclid_scores.csv', index=False)
+        cos_result_df.to_csv(f'./{model}_cosine_scores.csv', index=False)
+        euclid_result_df.to_csv(f'./{model}_euclid_scores.csv', index=False)
     
     current_df = cos_result_df if sim_measure == 'cosine' else euclid_result_df
     top_results = current_df.sort_values(by='Similarity', ascending=False if sim_measure == 'cosine' else True).head(int(top_n))
